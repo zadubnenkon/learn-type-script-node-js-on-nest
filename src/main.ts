990b5bb336d2fcd3aa55e6d25048749f21447167
zadubnenkon <zadubnenkon@yandex.ru>
2022-02-04 12:04:01 +0500

    prepare for Heroku

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import {ValidationPipe} from "./pipes/validation.pipe";
 
 
 async function start() {
-    const PORT = process.env.PORT || 5000;
+    const PORT = process.env.PORT || 8080;
     const app = await NestFactory.create(AppModule)
 
     const config = new DocumentBuilder()
